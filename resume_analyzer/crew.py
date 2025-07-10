5f1761f1840d1f033c9c5cb6f0bcfd9507b8a808
Matty <madesgputra@gmail.com>
2025-07-10 21:41:39 +0700

    :zap: Adding agents

--- resume_analyzer/crew.py
+++ resume_analyzer/crew.py
@@ -3,7 +3,7 @@ from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
 
-from crewai_tools import FileReadTool
+from crewai_tools import FileReadTool, SerperDevTool
 
 @CrewBase
 class ResumeAnalyzer():
@@ -12,20 +12,57 @@ class ResumeAnalyzer():
         self.agents: List[BaseAgent]
         self.tasks: List[Task]
         self.read_resume = FileReadTool(file_path='./fake_resume.md')
+        self.search_tool = SerperDevTool()
 
+    ################# List of Agents #################
     @agent
     def researcher(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
-            tools = [self.read_resume],
+            config=self.agents_config['researcher'], 
+            tools = [self.read_resume]
+        )
+    @agent
+    def profiler(self) -> Agent:
+        return Agent(
+            config=self.agents_config['profiler'], 
+            tools = [self.read_resume, self.search_tool]
+        )
+    @agent
+    def resume_strategist(self) -> Agent:
+        return Agent(
+            config=self.agents_config['resume_strategist'], 
+            tools = [self.read_resume, self.search_tool]
+        )
+    @agent
+    def interview_preparer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['interview_preparer'], 
+            tools = [self.read_resume, self.search_tool]
         )
     
+    ################# List of Task #################
     @task
     def research_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['research_task'], 
+        )
+    @task
+    def profile_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['profile_task'], 
+        )
+    @task
+    def resume_strategist_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['resume_strategist_task'], 
+        )
+    @task
+    def interview_preparation_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['interview_preparation_task']
         )
 
+    ################# Creating Crew #################
     @crew
     def crew(self) -> Crew:
         """Creates the ResumeAnalyzer crew"""
@@ -33,7 +70,7 @@ class ResumeAnalyzer():
         return Crew(
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
+            # process=Process.asynchronous
             verbose=True,
             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
